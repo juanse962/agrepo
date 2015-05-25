d326ec8ce1f6aa49e91d27a951c0ffd32de440ae
juanse962 <juanseg11@hotmail.com>
2015-05-24 21:09:20 -0500

    Create height.java

---
+++ height.java
@@ -0,0 +1,17 @@
+public class tree {
+	Node root 
+	public tree (){
+		root = null;
+	}
+}
+private int maxHeAUX(Node node){
+	int max= 0;
+	if(node != null)
+		for(int i=0; i<node.childrnsize();i++)
+			max = Math.max(mazHAUX(node.children.get(i))+1,);		
+			return max;
+		}
+	public int maxHeight(){
+		return maxHeAUX;
+	}
+}
